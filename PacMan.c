fb28531e277287b3378b1021e64bd85296ef6f59
willowhughes <willow.n.hughes@gmail.com>
2023-12-10 21:59:49 -1000

    score_system

--- PacMan.c
+++ PacMan.c
@@ -4,7 +4,17 @@
 #include<windows.h>
 #include<conio.h>
 
-void draw(char grid[10][10], int i, int j) {
+void pacmanAnimation(char grid[10][10], int row, int col, int i, int j, int score) {
+    grid[row][col] = 'C';
+    draw(grid, i, j, score);
+    grid[row][col] = 'O';
+    draw(grid, i, j, score);
+    Sleep(100);
+    grid[row][col] = 'C';
+    draw(grid, i, j, score);
+}
+
+void draw(char grid[10][10], int i, int j, int score) {
     system("cls");
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
@@ -12,29 +22,37 @@ void draw(char grid[10][10], int i, int j) {
         }
         printf("\n");
     }
+    printf("Score: %d ", score);
 }
 
 int main() {
 
+    //make generate map here instead
     char grid[10][10] = {
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'} ,
-        {'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'}
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'} ,
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'} ,
+        {'.', 'X', '.', '.', 'X', '.', '.', '.', '.', '.'} ,
+        {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'} ,
+        {'.', '.', 'X', '.', '.', '.', '.', '.', '.', '.'} ,
+        {'#', '#', '#', '#', '.', '.', '.', '.', '.', '.'} ,
+        {'.', '.', '.', '#', '.', '.', '.', '.', 'X', '.'} ,
+        {'.', '.', '.', '#', '#', '#', '#', '#', '#', '.'} ,
+        {'.', '.', '.', '.', '.', '.', '.', '.', '#', '.'} ,
+        {'.', '.', '.', '.', '.', '.', '.', '.', '#', '.'}
     };
+
+
 
     int x = 0;
     int row = 5;
     int col = 5;
     char input;
+    int score = 0;
     int i;
     int j;
+
+    grid[row][col] = 'C';
+    draw(grid, i, j, score);
 
     while (1) {
         if (_kbhit()) {
@@ -42,39 +60,53 @@ int main() {
 
             switch (input) {
                 case 'w':
-                    if (row > 0) {
+                    if (row > 0 && grid[row - 1][col] != '#') {
+                        if (grid[row - 1][col] == '.') {
+                            score++;
+                        }
                         grid[row][col] = ' ';
                         row--;
                     }
                 break;
                 case 'a':
-                    if (col > 0) {
+                    if (col > 0 && grid[row][col - 1] != '#') {
+                        if (grid[row][col - 1] == '.') {
+                            score++;
+                        }
                         grid[row][col] = ' ';
                         col--;
                     }
                 break;
                 case 's':
-                    if (row < 9) {
+                    if (row < 9 && grid[row + 1][col] != '#') {
+                        if (grid[row + 1][col] == '.') {
+                            score++;
+                        }
                         grid[row][col] = ' ';
                         row++;
                     }
                 break;
                 case 'd':
-                    if (col < 9) {
+                    if (col < 9 && grid[row][col + 1] != '#') {
+                        if (grid[row][col + 1] == '.') {
+                            score++;
+                        }
                         grid[row][col] = ' ';
                         col++;
                     }
                 break;
                 case 'q':
                     return 0;
-                }
-            grid[row][col] = 'C';
-            draw(grid, i, j);
-            grid[row][col] = 'O';
-            draw(grid, i, j);
-            Sleep(100);
-            grid[row][col] = 'C';
-            draw(grid, i, j);
+            }
+
+            if (grid[row][col] == 'X') {
+                pacmanAnimation(grid, row, col, i, j, score);
+                printf("\nGAME OVER");
+                return 0;
+            } else {
+                pacmanAnimation(grid, row, col, i, j, score);
+            }
+            
         }
     }
 }
